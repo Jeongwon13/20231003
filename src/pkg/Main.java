8dcf10116d3e92f458abd9a42c0451f363bac6c8
Jeongwon Choi <chlwjddnjs17@naver.com>
2023-10-03 19:11:51 +0900

    인프런 자바 알고리즘 / 임시반장 정하기

---
+++ src/pkg/Main.java
@@ -0,0 +1,44 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public int solution(int n, int[][] arr) {
+		int answer = 0, min = Integer.MIN_VALUE;
+		
+		for(int i=1; i<=n; i++) {
+			int cnt = 0;
+			for(int j=1; j<=n; j++) {
+				if(i!=j) {
+					for(int k=1; k<=n; k++) {
+						if(arr[i][k]==arr[j][k]) {
+							cnt++;
+							break;
+						}
+					}
+				}
+			}
+			if(cnt>min) {
+				min=cnt;
+				answer=i;
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n+1][n+1];
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print(T.solution(n, arr));
+		
+	}
+
+}
